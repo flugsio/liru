326558ed0e145527f472e6fed96a7d2a09a8a805
Jimmie Elvenmark <flugsio@gmail.com>
2015-07-09 16:20:07 +0200

    move ui stuff to own module and structure

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 use std::io::stdin;
 use uuid::Uuid;
-use time::Duration;
 
 use std::process::Command;
 
@@ -48,22 +47,9 @@ fn main() {
     match pov {
         Some(pov) => {
             let pov1 = Arc::new(Mutex::new(pov));
-            // TODO: clean this
-            let rustbox = tui::init();
             game::socket::connect(base_socket_url, sri, pov1.clone());
-            loop {
-                tui::render_fen(&rustbox, pov1.clone());
-                match rustbox.peek_event(Duration::milliseconds(100), false) {
-                    Ok(rustbox::Event::KeyEvent(key)) => {
-                        match key {
-                            Some(Key::Char('q')) => { break; }
-                            _ => { }
-                        }
-                    },
-                    Err(e) => panic!("{}", e),
-                    _ => { }
-                }
-            }
+            let mut ui = tui::UI::new(pov1.clone());
+            ui.start();
         },
         None => ()
     }
@@ -81,14 +67,9 @@ mod tui {
     use rustbox::Key;
     use rustbox::{RB_BOLD, RB_NORMAL};
 
-    use game;
+    use time::Duration;
 
-    pub fn init() -> RustBox {
-        match RustBox::init(Default::default()) {
-            Result::Ok(v) => v,
-            Result::Err(e) => panic!("{}", e),
-        }
-    }
+    use game;
 
     #[derive(Clone, Copy)]
     struct RBStyle {
@@ -97,60 +78,105 @@ mod tui {
         bg: Color,
     }
 
-    pub fn render_fen(rb: &RustBox, pov: Arc<Mutex<game::Pov>>) {
-        let pov = pov.lock().unwrap();
-        let fen = pov.game.fen.clone();
+    pub struct UI {
+        running: bool,
+        rb: RustBox,
+        povs: Vec<Arc<Mutex<game::Pov>>>
+    }
 
-        let border = RBStyle { style: RB_NORMAL, fg: Color::Cyan, bg: Color::Black };
-        let piece_dark = RBStyle { style: RB_BOLD, fg: Color::Blue, bg: Color::Black };
-        let piece_light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
-        let space_dark = RBStyle { style: RB_BOLD, fg: Color::Blue, bg: Color::Black };
-        let space_light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
+    impl UI {
+        pub fn new(pov: Arc<Mutex<game::Pov>>) -> UI {
 
-        rb.print(3,  1, RB_BOLD, Color::White, Color::Black, &fen);
-        rb.print(5,  3, border.style, border.fg, border.bg, "╔═════════════════╗");
-        rb.print(5, 12, border.style, border.fg, border.bg, "╚═════════════════╝");
+            let rb = match RustBox::init(Default::default()) {
+                Result::Ok(v) => v,
+                Result::Err(e) => panic!("{}", e),
+            };
 
-        // draw clocks
-        match pov.clock.as_ref() {
-            Some(clock) => {
-                self.rb.print(18,  2, RB_BOLD, if clock.black < 10f64 { Color::Red } else { Color::White }, Color::Black, &format!(" {:04.1}  ", clock.black));
-                self.rb.print(18,  13, RB_BOLD, if clock.white < 10f64 { Color::Red } else { Color::White }, Color::Black, &format!(" {:04.1}  ", clock.white));
-            },
-            None => ()
+            let mut povs = Vec::new();
+            povs.push(pov);
+
+            return UI {
+                running: true,
+                rb: rb,
+                povs: povs,
+            };
         }
-        // TODO: fen parser
-        // r1bqkb1r/ppp1pppp/2n2n2/3p4/4P3/3P1P2/PPP3PP/RNBQKBNR w KQkq - 1 4
-        for (y, row) in fen.split(' ').next().unwrap().split('/').enumerate() {
-            let row = row
-                .replace("8", "········")
-                .replace("7", "·······")
-                .replace("6", "······")
-                .replace("5", "·····")
-                .replace("4", "····")
-                .replace("3", "···")
-                .replace("2", "··")
-                .replace("1", "·");
-            rb.print(3, 4 + y, border.style, border.fg, border.bg, &format!("{} ║", 9-(y+1)));
-            rb.print(23, 4 + y, border.style, border.fg, border.bg, "║");
-            for (x, char) in row.chars().enumerate() {
-                let color = if char == '·' {
-                    if (y + x) % 2 == 0 {
-                        space_light
-                    } else {
-                        space_dark
-                    }
-                } else {
-                    if char.is_uppercase() {
-                        piece_light
-                    } else {
-                        piece_dark
-                    }
-                };
-                rb.print(7 + x*2, 4 + y, color.style, color.fg, color.bg, &char.to_uppercase().collect::<String>());
+
+        pub fn start(&mut self) {
+            while self.running {
+                self.render_fen();
+                self.poll_keys();
             }
         }
-        rb.present();
+
+        pub fn poll_keys(&mut self) {
+            match self.rb.peek_event(Duration::milliseconds(100), false) {
+                Ok(rustbox::Event::KeyEvent(key)) => {
+                    match key {
+                        Some(Key::Char('q')) => self.running = false,
+                        _ => { }
+                    }
+                },
+                Err(e) => panic!("{}", e),
+                _ => { }
+            }
+        }
+
+        pub fn render_fen(&mut self) {
+            let pov = self.povs[0].lock().unwrap();
+            let fen = pov.game.fen.clone();
+
+            let border = RBStyle { style: RB_NORMAL, fg: Color::Cyan, bg: Color::Black };
+            let piece_dark = RBStyle { style: RB_BOLD, fg: Color::Blue, bg: Color::Black };
+            let piece_light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
+            let space_dark = RBStyle { style: RB_BOLD, fg: Color::Blue, bg: Color::Black };
+            let space_light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
+
+            self.rb.print(3,  1, RB_BOLD, Color::White, Color::Black, &fen);
+            self.rb.print(5,  3, border.style, border.fg, border.bg, "╔═════════════════╗");
+            self.rb.print(5, 12, border.style, border.fg, border.bg, "╚═════════════════╝");
+
+            // draw clocks
+            match pov.clock.as_ref() {
+                Some(clock) => {
+                    self.rb.print(18,  2, RB_BOLD, if clock.black < 10f64 { Color::Red } else { Color::White }, Color::Black, &format!(" {:04.1}  ", clock.black));
+                    self.rb.print(18,  13, RB_BOLD, if clock.white < 10f64 { Color::Red } else { Color::White }, Color::Black, &format!(" {:04.1}  ", clock.white));
+                },
+                None => ()
+            }
+            // TODO: fen parser
+            // r1bqkb1r/ppp1pppp/2n2n2/3p4/4P3/3P1P2/PPP3PP/RNBQKBNR w KQkq - 1 4
+            for (y, row) in fen.split(' ').next().unwrap().split('/').enumerate() {
+                let row = row
+                    .replace("8", "········")
+                    .replace("7", "·······")
+                    .replace("6", "······")
+                    .replace("5", "·····")
+                    .replace("4", "····")
+                    .replace("3", "···")
+                    .replace("2", "··")
+                    .replace("1", "·");
+                self.rb.print(3, 4 + y, border.style, border.fg, border.bg, &format!("{} ║", 9-(y+1)));
+                self.rb.print(23, 4 + y, border.style, border.fg, border.bg, "║");
+                for (x, char) in row.chars().enumerate() {
+                    let color = if char == '·' {
+                        if (y + x) % 2 == 0 {
+                            space_light
+                        } else {
+                            space_dark
+                        }
+                    } else {
+                        if char.is_uppercase() {
+                            piece_light
+                        } else {
+                            piece_dark
+                        }
+                    };
+                    self.rb.print(7 + x*2, 4 + y, color.style, color.fg, color.bg, &char.to_uppercase().collect::<String>());
+                }
+            }
+            self.rb.present();
+        }
     }
 }
 
