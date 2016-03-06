68ab62f691ecb68de02d969bb43b33b3630695f7
Jimmie Elvenmark <flugsio@gmail.com>
2016-03-07 00:43:01 +0100

    menu works, yay

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,11 @@ extern crate websocket;
 extern crate rustbox;
 extern crate time;
 
-use std::sync::{Arc, Mutex};
-use uuid::Uuid;
-
-use std::io::Read;
-use hyper::Client;
-use hyper::header::Connection;
-
-use hyper::header::{Headers, Accept, qitem};
-use hyper::mime::{Mime, TopLevel, SubLevel};
-
-use rustc_serialize::json;
-
 mod game;
 mod ui;
 
 fn main() {
-    let sri = Uuid::new_v4().to_simple_string();
-    let base_url = "en.lichess.org".to_string();
-    let base_socket_url = "socket.en.lichess.org".to_string();
-
     let mut ui = ui::UI::new();
-    let pov = get_pov(base_url, "tv/bullet".to_string());
-    match pov {
-        Some(pov) => {
-            let pov1 = Arc::new(Mutex::new(pov));
-            game::socket::connect(base_socket_url, sri, pov1.clone());
-            ui.add_game(pov1.clone());
-        },
-        None => println!("no pov")
-    }
     ui.start();
-
-}
-
-fn get_pov(base_url: String, game_id: String) -> Option<game::Pov> {
-    let url = format!("http://{}/{}", base_url, game_id);
-    let client = Client::new();
-    let mut body = String::new();
-    client.get(&*url)
-        .header(Connection::close())
-        .header(Accept(vec![qitem(Mime(TopLevel::Application, SubLevel::Ext("vnd.lichess.v1+json".to_owned()), vec![]))]))
-        .send()
-        .map(|mut res| {
-            res.read_to_string(&mut body);
-        });
-    // TODO: catch error and print
-    json::decode(&body).unwrap()
 }
 
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -21,8 +21,13 @@ struct RBStyle {
 
 trait View {
     fn name(&self) -> &str;
-    fn render(&self, ui: &mut rustbox::RustBox);
-    fn key_event(&mut self, key: rustbox::keyboard::Key);
+    fn render(&self, ui: &mut Renderer);
+    fn key_event(&mut self, key: rustbox::keyboard::Key) -> MenuResult;
+}
+
+enum MenuResult {
+    AddGameView { name: String, url: String },
+    None
 }
 
 struct MenuView {
@@ -45,9 +50,13 @@ pub enum MenuOption {
 
 pub struct UI {
     running: bool,
-    rb: RustBox,
+    renderer: Renderer,
     views: Vec<Box<View>>,
     current_view: usize,
+}
+
+struct Renderer {
+    rb: RustBox,
 }
 
 impl UI {
@@ -60,14 +69,19 @@ impl UI {
 
         let mut views = Vec::new();
         let mut menuOptions = Vec::new();
-        menuOptions.push(MenuOption::WatchTv {
-            name: "Bullet".to_string(),
-            url: "tv/bullet".to_string(),
-        });
-        menuOptions.push(MenuOption::WatchTv {
-            name: "Blitz".to_string(),
-            url: "tv/blitz".to_string(),
-        });
+        menuOptions.push(MenuOption::WatchTv { name: "Best".to_string(), url: "tv/best".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Bullet".to_string(), url: "tv/bullet".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Blitz".to_string(), url: "tv/blitz".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Classical".to_string(), url: "tv/classical".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Crazyhouse".to_string(), url: "tv/crazyhouse".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Chess 960".to_string(), url: "tv/chess960".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "King of the Hill".to_string(), url: "tv/kingOfTheHill".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Three-Check".to_string(), url: "tv/threeCheck".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Antichess".to_string(), url: "tv/antichess".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Atomic".to_string(), url: "tv/atomic".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Horde".to_string(), url: "tv/horde".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Racing Kings".to_string(), url: "tv/racingKings".to_string() });
+        menuOptions.push(MenuOption::WatchTv { name: "Computer".to_string(), url: "tv/computer".to_string() });
 
         views.push(Box::new(MenuView {
             menuOptions: menuOptions,
@@ -76,24 +90,18 @@ impl UI {
 
         return UI {
             running: true,
-            rb: rb,
+            renderer: Renderer { rb: rb },
             views: views,
             current_view: 0,
         };
     }
 
-    //pub fn add_view(&mut self, view: View) {
-        //self.views.push(view);
-    //}
+    pub fn add_view(&mut self, view: Box<View>) {
+        self.views.push(view);
+    }
 
-    pub fn add_game(&mut self, pov: Arc<Mutex<game::Pov>>) {
-        let mut povs = Vec::new();
-        povs.push(pov);
-        self.views.push(Box::new(GameView {
-            name: "Bullet".to_string(),
-            url: "".to_string(),
-            povs: povs,
-        }) as Box<View>);
+    pub fn add_game(&mut self, name: String, url: String) {
+        self.add_view(Box::new(GameView::new(name, url)) as Box<View>);
     }
 
     pub fn start(&mut self) {
@@ -106,14 +114,14 @@ impl UI {
     fn prev_view(&mut self) {
         if 0 < self.current_view {
             self.current_view -= 1;
-            self.rb.clear();
+            self.renderer.clear();
         }
     }
 
     fn next_view(&mut self) {
         if self.current_view < self.views.len() - 1 {
             self.current_view += 1;
-            self.rb.clear();
+            self.renderer.clear();
         }
     }
 
@@ -122,13 +130,20 @@ impl UI {
     }
 
     pub fn poll_keys(&mut self) {
-        match self.rb.peek_event(Duration::milliseconds(100), false) {
+        match self.renderer.rb.peek_event(Duration::milliseconds(100), false) {
             Ok(rustbox::Event::KeyEvent(key)) => {
                 match key {
                     Some(Key::Char('q')) => self.running = false,
                     Some(Key::Left) => self.prev_view(),
                     Some(Key::Right) => self.next_view(),
-                    Some(key) => { self.current_view().key_event(key); }
+                    Some(key) => {
+                        match self.current_view().key_event(key) {
+                            MenuResult::AddGameView { name, url } => {
+                                self.add_game(name, url);
+                            }
+                            MenuResult::None => {},
+                        }
+                    }
                     _ => {}
                 }
             },
@@ -137,74 +152,88 @@ impl UI {
         }
     }
 
+    pub fn render(&mut self) {
+        let dark  = RBStyle { style: RB_NORMAL, fg: Color::Blue, bg: Color::Black };
+        let light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
+
+        self.renderer.print(0, 0, dark, "|");
+        let mut x = 2;
+        for (i, view) in self.views.iter().enumerate() {
+            if i == self.current_view {
+                self.renderer.print(x, 0, light, view.name());
+            } else {
+                self.renderer.print(x, 0, dark, view.name());
+            }
+            x += view.name().len() + 3;
+            self.renderer.print(x- 2, 0, dark, "|");
+        }
+
+        self.views.get(self.current_view).unwrap().render(&mut self.renderer);
+        self.renderer.present();
+    }
+
+}
+
+impl Renderer {
     pub fn print(&self, x: usize, y: usize, rbstyle: RBStyle, chars: &str) {
         self.rb.print(x, y, rbstyle.style, rbstyle.fg, rbstyle.bg, chars);
     }
 
-    pub fn render(&mut self) {
-        let dark  = RBStyle { style: RB_BOLD, fg: Color::Blue, bg:     Color::Black };
-        let light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
-        for (i, view) in self.views.iter().enumerate() {
-            if i == self.current_view {
-                self.print(i*10, 0, light, view.name());
-            } else {
-                self.print(i*10, 0, dark, view.name());
-            }
-        }
-
-        self.views.get(self.current_view).unwrap().render(&mut self.rb);
-        self.rb.present();
+    pub fn clear(&self) {
+        self.rb.clear();
     }
 
-    // #todo
-    //fn render_view(&mut self, view: &View) {
-    //}
-
+    pub fn present(&self) {
+        self.rb.present();
+    }
 }
 
 impl View for MenuView {
-    fn render(&self, rb: &mut rustbox::RustBox) {
+    fn render(&self, r: &mut Renderer) {
         let dark  = RBStyle { style: RB_BOLD, fg: Color::Blue, bg:     Color::Black };
         let light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
 
         for (i, option) in self.menuOptions.iter().enumerate() {
             if i == self.current {
-                self.print(rb, 0, 2 + i, light, option.name());
+                r.print(0, 2 + i, light, option.name());
             } else {
-                self.print(rb, 0, 2 + i, dark, option.name());
+                r.print(0, 2 + i, dark, option.name());
             }
         }
     }
-
 
     fn name(&self) -> &str {
         "Menu"
     }
 
-    fn key_event(&mut self, key: rustbox::keyboard::Key) {
+    fn key_event(&mut self, key: rustbox::keyboard::Key) -> MenuResult {
         match key {
             Key::Up => {
                 if 0 < self.current {
                     self.current -= 1;
                 }
+                MenuResult::None
             },
             Key::Down => {
                 if self.current < self.menuOptions.len() -1 {
                     self.current += 1;
                 }
+                MenuResult::None
             },
             Key::Enter => {
+                let option = self.menuOptions.get(self.current).unwrap(); // TODO
+                option.execute()
             }
-            _ => {}
+            _ => { MenuResult::None }
         }
     }
 }
 
 impl View for GameView {
-    fn render(&self, rb: &mut rustbox::RustBox) {
+    fn render(&self, r: &mut Renderer) {
         for (i, pov) in self.povs.iter().enumerate() {
             pov.lock().map(|p| {
-                self.render_pov(rb, i * 30, 0, &p);
+                self.render_pov(r, i * 30, 0, &p);
             });
         }
     }
@@ -213,53 +242,100 @@ impl View for GameView {
         &self.name
     }
 
-    fn key_event(&mut self, key: rustbox::keyboard::Key) {
-        // nothing yet
+    fn key_event(&mut self, key: rustbox::keyboard::Key) -> MenuResult {
+        MenuResult::None
     }
 }
 
 impl MenuView {
-    // TODO: where to put this, custom wrapper maybe
-    fn print(&self, rb: &mut rustbox::RustBox, x: usize, y: usize, rbstyle: RBStyle, chars: &str) {
-        rb.print(x, y, rbstyle.style, rbstyle.fg, rbstyle.bg, chars);
-    }
 }
 
+
+// TODO: move all this
+//use std::sync::{Arc, Mutex};
+use uuid::Uuid;
+
+use std::io::Read;
+use hyper::Client;
+use hyper::header::Connection;
+
+use hyper::header::{Headers, Accept, qitem};
+use hyper::mime::{Mime, TopLevel, SubLevel};
+
+use rustc_serialize::json;
+
 impl GameView {
-    fn print(&self, rb: &mut rustbox::RustBox, x: usize, y: usize, rbstyle: RBStyle, chars: &str) {
-        rb.print(x, y, rbstyle.style, rbstyle.fg, rbstyle.bg, chars);
+    pub fn new(name: String, url: String) -> GameView {
+        // TODO: move this
+        fn get_pov(base_url: String, game_id: String) -> Option<game::Pov> {
+            let url = format!("http://{}/{}", base_url, game_id);
+            let client = Client::new();
+            let mut body = String::new();
+            client.get(&*url)
+                .header(Connection::close())
+                .header(Accept(vec![qitem(Mime(TopLevel::Application, SubLevel::Ext("vnd.lichess.v1+json".to_owned()), vec![]))]))
+                .send()
+                .map(|mut res| {
+                    res.read_to_string(&mut body);
+                });
+            // TODO: catch error and print
+            json::decode(&body).unwrap()
+        }
+
+        // TODO: should this be reused or new for each socket?
+        let sri = Uuid::new_v4().to_simple_string();
+        let base_url = "en.lichess.org".to_string();
+        let base_socket_url = "socket.en.lichess.org".to_string();
+
+        let mut povs = Vec::new();
+
+        let pov = get_pov(base_url, url.clone());
+        match pov {
+            Some(pov) => {
+                let pov1 = Arc::new(Mutex::new(pov));
+                game::socket::connect(base_socket_url, sri, pov1.clone());
+                povs.push(pov1);
+            },
+            None => println!("no pov")
+        }
+
+        return GameView {
+            name: name,
+            url: url,
+            povs: povs,
+        };
     }
 
-    pub fn render_pov(&self, rb: &mut rustbox::RustBox, x: usize, y: usize, pov: &game::Pov) {
-        self.render_player(rb, x + 1, y + 2, &pov.opponent);
-        self.render_player(rb, x + 1, y + 14, &pov.player);
+    pub fn render_pov(&self, r: &mut Renderer, x: usize, y: usize, pov: &game::Pov) {
+        self.render_player(r, x + 1, y + 2, &pov.opponent);
+        self.render_player(r, x + 1, y + 14, &pov.player);
         let fen = pov.game.fen.clone();
-        self.render_fen(rb, x, y, fen);
+        self.render_fen(r, x, y, fen);
         match pov.clock {
             Some(ref clock) => {
-                self.render_clock(rb, x + 19, y + 3, clock.black);
-                self.render_clock(rb, x + 19, y + 12, clock.white);
+                self.render_clock(r, x + 19, y + 3, clock.black);
+                self.render_clock(r, x + 19, y + 12, clock.white);
             },
             None => ()
         };
     }
 
-    pub fn render_player(&self, rb: &mut rustbox::RustBox, x: usize, y: usize, player: &game::Player) {
+    pub fn render_player(&self, r: &mut Renderer, x: usize, y: usize, player: &game::Player) {
         let style = RBStyle { style: RB_BOLD, fg: Color::White, bg: Color::Black };
-        self.print(rb, x, y, style, &format!("{:4}", player.rating.unwrap_or(1500)));
+        r.print(x, y, style, &format!("{:4}", player.rating.unwrap_or(1500)));
         match player.user {
             Some(ref user) => {
-                self.print(rb, x + 5, y, style, &format!("{}", user.username));
+                r.print(x + 5, y, style, &format!("{}", user.username));
             },
             None => ()
         };
     }
 
-    pub fn render_clock(&self, rb: &mut rustbox::RustBox, x: usize, y: usize, time: f64) {
-        self.print(rb, x, y, RBStyle { style: RB_BOLD, fg: if time < 10f64 { Color::Red } else { Color::White }, bg: Color::Black }, &format!("{:04.1}", time));
+    pub fn render_clock(&self, r: &mut Renderer, x: usize, y: usize, time: f64) {
+        r.print(x, y, RBStyle { style: RB_BOLD, fg: if time < 10f64 { Color::Red } else { Color::White }, bg: Color::Black }, &format!("{:04.1}", time));
     }
 
-    pub fn render_fen(&self, rb: &mut rustbox::RustBox, x: usize, y: usize, fen: String) {
+    pub fn render_fen(&self, r: &mut Renderer, x: usize, y: usize, fen: String) {
         let text_style  = RBStyle { style: RB_BOLD, fg: Color::White, bg:    Color::Black };
         let border      = RBStyle { style: RB_NORMAL, fg: Color::Cyan, bg:   Color::Black };
         let piece_dark  = RBStyle { style: RB_BOLD, fg: Color::Blue, bg:     Color::Black };
@@ -267,10 +343,10 @@ impl GameView {
         let space_dark  = RBStyle { style: RB_BOLD, fg: Color::Blue, bg:     Color::Black };
         let space_light = RBStyle { style: RB_NORMAL, fg: Color::Yellow, bg: Color::Black };
 
-        self.print(rb, x + 3, y +  1, text_style, &fen);
-        self.print(rb, x + 5, y +  3, border, "╔═════════════════╗");
-        self.print(rb, x + 5, y + 12, border, "╚═════════════════╝");
-        self.print(rb, x + 7, y + 13, border,   "A B C D E F G H");
+        //r.print(x + 3, y +  1, text_style, &fen);
+        r.print(x + 5, y +  3, border, "╔═════════════════╗");
+        r.print(x + 5, y + 12, border, "╚═════════════════╝");
+        r.print(x + 7, y + 13, border,   "A B C D E F G H");
 
         // TODO: fen parser
         // r1bqkb1r/ppp1pppp/2n2n2/3p4/4P3/3P1P2/PPP3PP/RNBQKBNR w KQkq - 1 4
@@ -284,8 +360,8 @@ impl GameView {
                 .replace("3", "···")
                 .replace("2", "··")
                 .replace("1", "·");
-            self.print(rb, x + 3, 4 + y + y2, border, &format!("{} ║", 9-(y2+1)));
-            self.print(rb, x + 23, 4 + y + y2, border, "║");
+            r.print(x + 3, 4 + y + y2, border, &format!("{} ║", 9-(y2+1)));
+            r.print(x + 23, 4 + y + y2, border, "║");
             for (x2, char) in row.chars().enumerate() {
                 let color = if char == '·' {
                     if (y2 + x2) % 2 == 0 {
@@ -300,7 +376,7 @@ impl GameView {
                         piece_dark
                     }
                 };
-                self.print(rb, 7 + x + x2*2, 4 + y + y2, color, &char.to_uppercase().collect::<String>());
+                r.print(7 + x + x2*2, 4 + y + y2, color, &char.to_uppercase().collect::<String>());
             }
         }
     }
@@ -315,6 +391,12 @@ impl MenuOption {
         }
     }
 
-    pub fn execute(&self) {
+    pub fn execute(&self) -> MenuResult {
+        match self {
+            &MenuOption::WatchTv {ref name, ref url} => {
+                MenuResult::AddGameView { name: name.clone(), url: url.clone() }
+            },
+            //_ => { MenuResult::None }
+        }
     }
 }
